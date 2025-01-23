64957c5036ef9706293529a61d9d1856d1c21b22
Jef Jonkers <26346574+jejon@users.noreply.github.com>
2025-01-23 18:28:42 +0100

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="crepes_weighted",
-    version="0.1.0",
+    version="0.1.1",
     author="Jef Jonkers",
     author_email="jef.jonkers@ugent.be",
     description="Weighted version of crepes: Conformal classifiers, regressors, and predictive systems (crepes)",
